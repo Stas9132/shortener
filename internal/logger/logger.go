0bb18cbbbda2bc4098a5459982b8439d55217448
stas9132 <ssgolobokov@gmail.com>
2023-09-21 13:19:09 +0300

    add response logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -17,14 +17,42 @@ func Initialize(level string) error {
 	return nil
 }
 
+type responseData struct {
+	status int
+	size   int
+}
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
 func RequestLogger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t := time.Now()
-		h.ServeHTTP(w, r)
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   &responseData{},
+		}
+		h.ServeHTTP(lw, r)
 		Log.WithFields(logrus.Fields{
 			"uri":      r.URL.RequestURI(),
 			"method":   r.Method,
 			"duration": time.Since(t),
 		}).Infoln("Request")
+		Log.WithFields(logrus.Fields{
+			"status": lw.responseData.status,
+			"size":   lw.responseData.size,
+		}).Infoln("Response")
 	})
 }
