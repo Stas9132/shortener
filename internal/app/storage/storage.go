86d343c2c00b1583639f1636cb1e5dacf4622be8
stas9132 <ssgolobokov@gmail.com>
2023-10-17 18:49:53 +0300

    add postgres support

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -12,7 +12,7 @@ import (
 
 type StorageT struct {
 	cache map[any]any
-	file  *os.File
+	file  struct{}
 	db    *sql.DB
 }
 
@@ -26,66 +26,114 @@ type StorageI interface {
 
 func New() *StorageT {
 	c := make(map[any]any)
+	var d *sql.DB
 
-	f, err := os.OpenFile(*config.FileStoragePath, os.O_CREATE|os.O_RDWR, 0644)
-	if err != nil {
-		logger.WithField("error", err).Errorln("Error while open file")
-	} else {
-		var fd []FileStorageRecordT
-		if err = json.NewDecoder(f).Decode(&fd); err != nil {
-			logger.WithField("error", err).Errorln("Error while decode json")
-		} else {
-			for _, record := range fd {
-				c[record.ShortURL] = record.OriginalURL
-			}
+	switch {
+	case len(*config.DatabaseDsn) == 0:
+		b, err := os.ReadFile(*config.FileStoragePath)
+		if err != nil {
+			logger.WithField("error", err).Errorln("Error while read file")
 		}
+		var fd []FileStorageRecordT
+		if err = json.Unmarshal(b, &fd); err != nil {
+			logger.WithField("error", err).Errorln("Error while unmarshal json")
+		}
+		for _, record := range fd {
+			c[record.ShortURL] = record.OriginalURL
+		}
+	case len(*config.DatabaseDsn) > 0:
+		db, err := sql.Open("pgx", *config.DatabaseDsn)
+		if err != nil {
+			logger.WithField("error", err).Errorln("Error while open db")
+		}
+		d = db
+		_, _ = db.Exec("create table shortener(id serial primary key , short_url varchar(255) unique not null, original_url varchar(255))")
 	}
-
-	db, err := sql.Open("pgx", *config.DatabaseDsn)
-	if err != nil {
-		logger.WithField("error", err).Errorln("Error while open db")
-	}
-
 	return &StorageT{
 		cache: c,
-		file:  f,
-		db:    db,
+		file:  struct{}{},
+		db:    d,
 	}
 }
 
 func (s *StorageT) Load(key any) (value any, ok bool) {
-	v, ok := s.cache[key]
-	return v, ok
+	switch {
+	case len(*config.DatabaseDsn) == 0:
+		value, ok = s.cache[key]
+	case len(*config.DatabaseDsn) > 0:
+		err := s.db.QueryRow("SELECT original_url FROM shortener WHERE short_url = $1", key.(string)).
+			Scan(&value)
+		if err != nil {
+			return "", false
+		}
+		ok = true
+	}
+	return
 }
 
 func (s *StorageT) Store(key, value any) {
-	s.cache[key] = value
-	_, _ = s.file.Seek(0, 0)
-	var fd []FileStorageRecordT
-	if err := json.NewDecoder(s.file).Decode(&fd); err != nil {
-		logger.WithField("error", err).Errorln("Error while decode json")
-	}
-	fd = append(fd, FileStorageRecordT{
-		UUID:        uuid.NewString(),
-		ShortURL:    key.(string),
-		OriginalURL: value.(string),
-	})
-	_, _ = s.file.Seek(0, 0)
-	if err := json.NewEncoder(s.file).Encode(fd); err != nil {
-		logger.WithField("error", err).Errorln("Error while encode json")
-	}
-}
-
-func (s *StorageT) Range(f func(key, value any) bool) {
-	for k, v := range s.cache {
-		if !f(k, v) {
-			break
+	switch {
+	case len(*config.DatabaseDsn) == 0:
+		b, err := os.ReadFile(*config.FileStoragePath)
+		if err != nil {
+			logger.WithField("error", err).Errorln("Error while read file")
+		}
+		var fd []FileStorageRecordT
+		if err = json.Unmarshal(b, &fd); err != nil {
+			logger.WithField("error", err).Errorln("Error while unmarshal json")
+		}
+		fd = append(fd, FileStorageRecordT{
+			UUID:        uuid.NewString(),
+			ShortURL:    key.(string),
+			OriginalURL: value.(string),
+		})
+		if b, err = json.Marshal(fd); err != nil {
+			logger.WithField("error", err).Errorln("Error while marshal json")
+		}
+		if err = os.WriteFile(*config.FileStoragePath, b, 0644); err != nil {
+			logger.WithField("error", err).Errorln("Error while write file")
+		}
+	case len(*config.DatabaseDsn) > 0:
+		_, err := s.db.Exec("INSERT INTO shortener(short_url,original_url) values ($1, $2)", key.(string), value.(string))
+		if err != nil {
+			logger.WithField("error", err).
+				Warningln("Error while insert data")
 		}
 	}
 }
 
+func (s *StorageT) Range(f func(key, value any) bool) {
+	switch {
+	case len(*config.DatabaseDsn) == 0:
+		for k, v := range s.cache {
+			if !f(k, v) {
+				break
+			}
+		}
+	case len(*config.DatabaseDsn) > 0:
+		rows, err := s.db.Query("SELECT short_url, original_url FROM shortener")
+		if err != nil {
+			logger.WithField("error", err).
+				Warningln("Error while select data")
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var key, value string
+			err = rows.Scan(&key, &value)
+			if err != nil {
+				logger.WithField("error", err).
+					Warningln("Error while select data")
+			}
+			if !f(key, value) {
+				break
+			}
+		}
+
+	}
+}
+
 func (s *StorageT) Close() error {
-	return s.file.Close()
+	return s.db.Close()
 }
 
 func (s *StorageT) Ping() error {
