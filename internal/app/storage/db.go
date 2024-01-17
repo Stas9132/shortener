492b60b703760d8518af2fde92e61fecf6138b22
stas9132 <ssgolobokov@gmail.com>
2024-01-17 17:00:05 +0300

    fix godoc

--- internal/app/storage/db.go
+++ internal/app/storage/db.go
@@ -16,6 +16,7 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
+// DBT struct
 type DBT struct {
 	appCtx context.Context
 	logger logger.Logger
@@ -23,6 +24,7 @@ type DBT struct {
 	m      *migrate.Migrate
 }
 
+// NewDB constructor
 func NewDB(ctx context.Context, l logger.Logger) (*DBT, error) {
 	db, err := sql.Open("pgx", *config.DatabaseDsn)
 	if err != nil {
@@ -60,6 +62,7 @@ func NewDB(ctx context.Context, l logger.Logger) (*DBT, error) {
 	}, nil
 }
 
+// Load - method
 func (s *DBT) Load(key string) (value string, ok bool) {
 	var b *bool
 	err := s.db.QueryRowContext(s.appCtx, "SELECT original_url, is_deleted FROM shortener WHERE short_url = $1", key).
@@ -74,6 +77,7 @@ func (s *DBT) Load(key string) (value string, ok bool) {
 	return
 }
 
+// StoreExt - method
 func (s *DBT) StoreExt(key, value, user string) {
 	_, err := s.db.ExecContext(s.appCtx, "INSERT INTO shortener(short_url,original_url, user_id) values ($1, $2, $3)", key, value, user)
 
@@ -85,22 +89,26 @@ func (s *DBT) StoreExt(key, value, user string) {
 	}
 }
 
+// Store - method
 func (s *DBT) Store(key, value string) {
 	s.StoreExt(key, value, uuid.NewString())
 }
 
+// LoadOrStore - method
 func (s *DBT) LoadOrStore(key, value string) (actual string, loaded bool) {
 	actual, loaded = s.Load(key)
 	s.Store(key, value)
 	return
 }
 
+// LoadOrStoreExt - method
 func (s *DBT) LoadOrStoreExt(key, value, user string) (actual string, loaded bool) {
 	actual, loaded = s.Load(key)
 	s.StoreExt(key, value, user)
 	return
 }
 
+// Range - method
 func (s *DBT) Range(f func(key, value string) bool) {
 	rows, err := s.db.QueryContext(s.appCtx, "SELECT short_url, original_url FROM shortener")
 	if err != nil || rows.Err() != nil {
@@ -121,6 +129,7 @@ func (s *DBT) Range(f func(key, value string) bool) {
 	}
 }
 
+// RangeExt - method
 func (s *DBT) RangeExt(f func(key, value, user string) bool) {
 	rows, err := s.db.QueryContext(s.appCtx, "SELECT short_url, original_url, user_id FROM shortener")
 	if err != nil || rows.Err() != nil {
@@ -142,6 +151,7 @@ func (s *DBT) RangeExt(f func(key, value, user string) bool) {
 
 }
 
+// Delete - method
 func (s *DBT) Delete(keys ...string) {
 	for _, key := range keys {
 		_, err := s.db.ExecContext(s.appCtx, "update shortener set is_deleted = true where short_url = $1", key)
@@ -151,11 +161,13 @@ func (s *DBT) Delete(keys ...string) {
 	}
 }
 
+// Close - method
 func (s *DBT) Close() error {
 	//return errors.Join(s.db.Close(), s.m.Down())
 	return s.db.Close()
 }
 
+// Ping - method
 func (s *DBT) Ping() error {
 	return s.db.Ping()
 }
